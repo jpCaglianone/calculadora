f055761f706450ed5ae34d6aaf347dadd8f37b81
jpCaglianone <89998145+jpCaglianone@users.noreply.github.com>
2022-11-12 05:42:37 +0000

    v1.1

---
+++ script.js
@@ -0,0 +1,48 @@
+
+//let temSinal = false
+//let temPonto = false
+// let clear
+
+visor.innerText = 0;
+let acumulador = ""; 
+
+function botaoUm(){
+  acumulador += "1"; // acumulador = acumulador + "1";
+  visor.innerText = acumulador;
+}
+function botaoDois(){
+  acumulador += "2"; 
+  visor.innerText = acumulador;
+}
+function botaoTres(){
+  acumulador += "3"; 
+  visor.innerText = acumulador;
+}
+function botaoQuatro(){
+  acumulador += "4"; 
+  visor.innerText = acumulador;
+}
+function botaoCinco(){
+  acumulador += "5"; 
+  visor.innerText = acumulador;
+}
+function botaoSeis(){
+  acumulador += "6"; 
+  visor.innerText = acumulador;
+}
+function botaoSete(){
+  acumulador += "7"; 
+  visor.innerText = acumulador;
+}
+function botaoOito(){
+  acumulador += "8"; 
+  visor.innerText = acumulador;
+}
+function botaoNove(){
+  acumulador += "9"; 
+  visor.innerText = acumulador;
+}
+function botaoZero(){
+  acumulador += "0"; 
+  visor.innerText = acumulador;
+}
